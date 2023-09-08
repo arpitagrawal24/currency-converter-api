98a6f3c827cb2dc303f3a613acfd99cb9b3f867c
Arpit <arpitagrawal2402@gmail.com>
2023-09-08 13:58:14 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,64 +1,59 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const port = 7850;
 
 app.use(express.json());
 
+const FIXER_API_KEY = "f3530d21f2adf8022111806fc8adbb1b";
+const FIXER_API_URL = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`;
 
-const FIXER_API_KEY = 'f3530d21f2adf8022111806fc8adbb1b';
-
-
-const FIXER_API_URL = 'https://data.fixer.io/api/latest';
-
-
-app.post('/convert', async (req, res) => {
+app.post("/convert", async (req, res) => {
   try {
-    const { toConvert } = req.body;
+    const { amount, from, to } = req.body;
+
+    // Make a request to the Fixer API to get exchange rates
+    const response = await axios.get(FIXER_API_URL);
+    const exchangeRates = response.data.rates;
+
     const conversions = [];
 
-    for (const conversion of toConvert) {
-      const { amount, from, to } = conversion;
-
-     
-      const response = await axios.get(FIXER_API_URL, {
-        params: {
-          access_key: FIXER_API_KEY,
-          base: from,
-        },
-      });
-
-      const exchangeRates = response.data.rates;
-      const exchangeValues = {};
-
-      for (const targetCurrency of to) {
-        if (exchangeRates[targetCurrency]) {
-          const convertedAmount = (amount * exchangeRates[targetCurrency]).toFixed(2);
-          exchangeValues[targetCurrency] = {
-            to: targetCurrency,
-            value: parseFloat(convertedAmount),
-          };
-        }
+    // Loop through the target currencies and calculate conversions
+    for (const targetCurrency of to) {
+      if (!exchangeRates[targetCurrency]) {
+        // Handle the case where the target currency is not available
+        conversions.push({
+          amount,
+          from,
+          to: targetCurrency,
+          error: `Currency ${targetCurrency} not found in exchange rates.`,
+        });
+        continue;
       }
+
+      const exchangeValue = amount * exchangeRates[targetCurrency];
 
       conversions.push({
         amount,
         from,
-        exchangeValues,
+        to: targetCurrency,
+        exchangeValue,
       });
     }
 
-    res.status(200).json({ conversions });
+    res.json({ conversions });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    // Handle any errors that occur during the conversion
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the request." });
   }
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Currency Converter API');
-  });
+app.get("/", (req, res) => {
+  res.send("Welcome to the Currency Converter API");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
